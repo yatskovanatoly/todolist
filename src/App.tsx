11d219f4f1c4f88c038eeb8758ccd18c300e4c0d
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-05-31 20:05:23 +0300

    undone

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, CssBaseline, Stack, Button } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { LightTheme, DarkTheme } from "./components/Themes";
+import { useState, useEffect } from "react";
+import { MaterialUISwitch } from "./components/Switch";
+import ToDos from "./components/ToDos";
+import { useMediaQuery } from "@mui/material";
+
+
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [mode, setMode] = useState(prefersDarkMode ? "dark" : "light");
+  useEffect(
+    () => setMode(prefersDarkMode ? "dark" : "light"),
+    [prefersDarkMode]
+  );
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Stack alignItems={"center"}>
+          <MaterialUISwitch
+            sx={{ m: 1, opacity: 0.7 }}
+            onChange={() => setMode(mode === "light" ? "dark" : "light")}
+            checked={mode === "light" ? false : true}
+          />
+          <h1>ToDo List:</h1>
+          <ToDos />
+        </Stack>
+      </Container>
+    </ThemeProvider>
   );
 }
 
