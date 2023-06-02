157547461d49868f84f988c17fa00cf7b3cf1101
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-06-02 18:05:06 +0300

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import "./App.css";
-import { Container, CssBaseline, Stack, Button } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  Stack,
+} from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { LightTheme, DarkTheme } from "./components/Themes";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { MaterialUISwitch } from "./components/Switch";
 import ToDos from "./components/ToDos";
 import { useMediaQuery } from "@mui/material";
-
-type mode = "dark" | "light"
+import { FormattedMessage } from "react-intl";
+import { Context } from "./components/Localisation";
+import LanguageChanger from "./components/LanguageChanger";
 
 function App() {
+  type mode = "dark" | "light";
+  const context = useContext(Context);
   const prefersDarkMode = useMediaQuery<string>("(prefers-color-scheme: dark)");
   const [mode, setMode] = useState<mode>(prefersDarkMode ? "dark" : "light");
   useEffect(
@@ -21,15 +28,25 @@ function App() {
     <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Stack alignItems={"center"}>
+        <Stack mt={1} direction={"row"} alignItems={"center"} justifyContent={'space-between'}>
+          <select value={context.locale} onChange={context.selectLanguage}>
+            <option value="ru">Русский</option>
+            <option value="en">English</option>
+            <option value="fr">Français</option>
+          </select>
+          {/* <LanguageChanger /> */}
           <MaterialUISwitch
-            sx={{ m: 1, opacity: 0.7 }}
+            sx={{ opacity: 0.7 }}
             onChange={() => setMode(mode === "light" ? "dark" : "light")}
             checked={mode === "light" ? false : true}
           />
-          <h1>ToDo List:</h1>
-          <ToDos />
         </Stack>
+        <Stack mt={15} mb={2} alignItems="center">
+          <h1>
+            <FormattedMessage id="title" defaultMessage="ToDo List:" />
+          </h1>
+        </Stack>
+        <ToDos />
       </Container>
     </ThemeProvider>
   );
